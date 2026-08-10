72e122556253b299c45c99cdf8ff282ffcb83c8a
tnghks339-netizen <tnghks339@gmail.com>
2026-08-10 15:00:12 +0900

    Update tabtest.js

--- tabtest.js
+++ tabtest.js
@@ -1,4 +1,4 @@
 (function () {
   var tabs = document.querySelectorAll('[role=tab]');
-  tabs.forEach (function(tab) { tab.style.outline = '3px solid red'; });
+  tabs.forEach (function(tab) { tab.style.outline = '3px solid blue'; });
 })();
